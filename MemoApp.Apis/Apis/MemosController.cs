3c79fc6b44fc26400e02b4980264afcaa3f6cf4c
RedPlus <redplus@redplus.net>
2020-06-20 21:08:44 +0900

    Web API를 위한 MemoApp.Apis 프로젝트 생성 및 종속성 주입 관련 기본 코드 작성

---
+++ MemoApp.Apis/Apis/MemosController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MemoApp.Apis.Controllers
+{
+    public class MemosController : ControllerBase
+    {
+
+    }
+}
---
+++ MemoApp.Apis/Startup.cs
@@ -0,0 +1,50 @@
+using MemoApp.Apis.Memos;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace MemoApp.Apis
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            /// <summary>
+            /// 메모앱(MemoApp) 관련 의존성(종속성) 주입 관련 코드만 따로 모아서 관리 
+            /// </summary>
+            services.AddDependencyInjectionContainerForMemoApp(Configuration);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
